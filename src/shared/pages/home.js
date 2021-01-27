f7b058b9e18170221c50baf47d22e043b17e79e6
tunedinout <narottam2911@gmail.com>
2021-01-28 00:34:02 +0530

    Profile picture component created

--- src/shared/pages/home.js
+++ src/shared/pages/home.js
@@ -1,41 +1,10 @@
 import React from 'react'
-import Group from '@material-ui/icons/Group'
-import KeyboardCapslockIcon from '@material-ui/icons/KeyboardCapslock';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
 import ButtonTitleBar from '../../places/components/ButtonedTitleBar';
 
-var buttons = [
-    {
-        color: "secondary",
-        text: "Users",
-        icon: <Group />,
-        variant: "contained",
-        path: "/users"
-    },
-    {
-        color: "Primary",
-        text: "Auth",
-        icon: <KeyboardCapslockIcon />,
-        variant: "contained",
-        path: "auth",
-    },
-    {
-        color: "tertiary",
-        text: "New",
-        icon: <AddCircleIcon />,
-        variant: "outlined",
-        path: "places/new"
-    }
-]
-const onUsersClick = (event) => {
 
-}
-const Home = () => {
-    // return <div className="button-titled-bar">
-    //     <ButtonTitleBar buttonList={buttons} title="Mappy" />
-    // </div>
-    return <>
-        <ButtonTitleBar buttonList={buttons} title="Mappy" />
-    </>
+const Home = (props) => {
+    return <div className="home-page">
+        <ButtonTitleBar buttonList={props.buttons} title={props.title} />
+    </div>
 }
 export default Home;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ButtonTitleBar from '../../places/components/ButtonedTitleBar';
+import UserListItem from '../components/UserListItem';
+import './Users.css'
 
-const Users = () => {
-    return <h1>
-        Users Works.
-    </h1>
+const Users = (props) => {
+    return <div className="users-page">
+        <ButtonTitleBar buttonList={props.buttons} title={props.title} />
+        <UserListItem />
+    </div>
+
 }
 
 export default Users;
